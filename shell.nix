6c9bc4cb54030955b8515b50f0a3164db60489a4
Sergey Mironov <grrwlf@gmail.com>
2020-02-07 16:13:12 +0300

    Add pylightnix dependency

--- shell.nix
+++ shell.nix
@@ -7,6 +7,29 @@ let
   pyls = py.python-language-server.override { providers=["pyflakes"]; };
   pyls-mypy = py.pyls-mypy.override { python-language-server=pyls; };
 
+
+  pylightnix_dev = import ../stagedml/3rdparty/pylightnix { inherit pkgs; doCheck = false; };
+
+  pylightnix_head = py.buildPythonPackage rec {
+    pname = "pylightinx";
+    version = "0.0.3";
+
+    preConfigure = with pkgs; ''
+      export PATH=${wget}/bin:${atool}/bin:$PATH
+    '';
+
+    checkInputs = with py; [ pytest pytest-mypy hypothesis ];
+
+    checkPhase = "pytest";
+
+    src = pkgs.fetchgit {
+      url = "https://github.com/stagedml/pylightnix";
+      sha256 = "sha256:0xqrmg9786yhk2if4fcmw4snw6cd6rk7hj28dz1s7idalip0wc71";
+    };
+  };
+
+
+
   env = stdenv.mkDerivation {
     name = "buildenv";
     buildInputs =
@@ -25,7 +48,10 @@ let
         matplotlib
         numpy
         tqdm
-        # scipy
+        scipy
+
+        # pylightnix_head
+        pylightnix_dev
       ]) ++ (with pkgs;[ gnumake ]);
 
     shellHook = with pkgs; ''
